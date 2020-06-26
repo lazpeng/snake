3c7833637cf01185c59219c215bf4df646aa712a
Rafael Nakano <rafael.nakano@maximatech.com.br>
2020-06-25 22:11:49 -0300

    Added grass; First level playable

--- game.js
+++ game.js
@@ -2,7 +2,7 @@ let width, height;
 let numSlotsHor = 26, numSlotsVer;
 let snakeStartingX, snakeStartingY;
 var gGame;
-let updateInterval, fixedInterval;
+let updateInterval;
 
 function getGridWidth() {
     return width / numSlotsHor;
@@ -20,6 +20,7 @@ class BodyPart {
         this.prevY = y;
         this.direction = direction;
         this.modifier = modifier;
+        this.moved = true;
     }
 }
 
@@ -50,14 +51,21 @@ class Snake {
         } else {
             head.y += head.modifier;
         }
-
+        if ((head.x == numSlotsHor || head.x < 0) || head.y == numSlotsVer || head.y < 0) {
+            die();
+        }
         for (let i = 1; i < this.bodyParts.length; ++i) {
             let part = this.bodyParts[i];
             let prev = this.bodyParts[i - 1];
+            if (head.x == part.x && head.y == part.y) {
+                die();
+            }
 
             part.x = prev.prevX;
             part.y = prev.prevY;
         }
+
+        this.moved = true;
     }
 
     draw(ctx) {
@@ -106,6 +114,10 @@ class Game {
         this.snake = new Snake();
         this.fruit = this.createFruit();
         this.state = 'paused';
+        this.count = 0;
+        this.elapsed = 0;
+        this.targetTime = 500;
+        this.minTarget = 75;
     }
 
     createFruit() {
@@ -119,27 +131,31 @@ class Game {
 
         switch (e.keyCode) {
             case 37:
-                if(head.direction != 'horizontal') {
+                if (head.direction != 'horizontal' && this.snake.moved) {
                     head.direction = 'horizontal';
                     head.modifier = -1;
+                    this.snake.moved = false;
                 }
                 break;
             case 38:
-                if(head.direction != 'vertical') {
+                if (head.direction != 'vertical' && this.snake.moved) {
                     head.direction = 'vertical';
                     head.modifier = -1;
+                    this.snake.moved = false;
                 }
                 break;
             case 39:
-                if(head.direction != 'horizontal') {
+                if (head.direction != 'horizontal' && this.snake.moved) {
                     head.direction = 'horizontal';
-                    head.modifier = +1;
+                    head.modifier = 1;
+                    this.snake.moved = false;
                 }
                 break;
             case 40:
-                if(head.direction != 'vertical') {
+                if (head.direction != 'vertical' && this.snake.moved) {
                     head.direction = 'vertical';
                     head.modifier = 1;
+                    this.snake.moved = false;
                 }
                 break;
             default: break;
@@ -148,51 +164,76 @@ class Game {
 
     eat() {
         let head = this.snake.bodyParts[0];
-
         if (head.x == this.fruit.x && head.y == this.fruit.y) {
             this.fruit = this.createFruit();
             let last = this.snake.bodyParts[this.snake.bodyParts.length - 1];
-            let x = last.direction == 'vertical' ? last.x : last.x - last.modifier;
-            let y = last.direction == 'horizontal' ? last.y : last.y - last.modifier;
-            let newLast = new BodyPart(x, y, last.direction, last.modifier);
+            let newLast = new BodyPart(last.prevX, last.prevY, last.direction, last.modifier);
             this.snake.bodyParts.push(newLast);
-        }
-    }
+            
+            document.querySelector('h1#fruit').innerText = `Comeu ${this.count += 1} frutinha${this.count > 1 ? 's' : ''}`;
 
-    fixedUpdate() {
-        if(this.state != 'running') {
-            return;
+            if(this.targetTime > this.minTarget && (this.count % 2) == 0) {
+                this.targetTime -= 25;
+            }
         }
-
-        this.snake.move();
-        this.eat();
     }
 }
 
-function gameUpdate(ctx, game) {
+function gameUpdate(ctx, game, deltaTime) {
     ctx.fillStyle = 'rgb(41, 41, 41)';
     ctx.fillRect(0, 0, width, height);
+
+    for(let row = 0; row < numSlotsVer; ++row) {
+        for(let col = 0; col < numSlotsHor; ++col) {
+            ctx.drawImage(document.getElementById('bg'), getGridWidth() * col, getGridHeight() * row, getGridWidth(), getGridHeight());
+        }
+    }
+
+    game.elapsed += deltaTime;
+
+    if(game.elapsed >= game.targetTime) {
+        game.elapsed = 0;
+
+        if(game.state == 'running') {
+            game.snake.move();
+            game.eat();
+        }
+    }
 
     game.fruit.draw(ctx);
     game.snake.draw(ctx);
 
-    if(gGame.state != 'running') {
+    if (gGame.state != 'running') {
         ctx.fillStyle = 'rgba(99, 99, 99, 0.5)';
         ctx.fillRect(0, 0, width, height);
 
         ctx.fillStyle = 'rgb(200, 200, 200)';
         ctx.font = '32px "Comic Sans"';
-        ctx.fillText('Paused', width / 2, height / 2);
+        let text;
+        if(gGame.state == 'paused') {
+            text = "Paused";
+        } else if(gGame.state == 'dead') {
+            text = "You (not Watanabe) are dead";
+        }
+        ctx.fillText(text, width / 2, height / 2);
     }
+}
+
+function die() {
+    let button = document.getElementById('startresume');
+
+    gGame.state = 'dead';
+    button.innerText = 'Start';
+    button.style = "display: none;";
 }
 
 function onStartResume() {
     let button = document.getElementById('startresume');
 
-    if(gGame !== undefined) {
-        if(gGame.state == 'running') {
+    if (gGame !== undefined) {
+        if (gGame.state == 'running') {
             gGame.state = 'paused';
-            button.innerText = 'Start';
+            button.innerText = 'Resume';
         } else {
             gGame.state = 'running';
             button.innerText = 'Pause';
@@ -203,6 +244,7 @@ function onStartResume() {
 function onRestart() {
     let button = document.getElementById('startresume');
     button.innerText = "Start";
+    button.style = "";
 
     gameStart();
 }
@@ -222,15 +264,10 @@ function gameStart() {
         let game = gGame = new Game();
         window.addEventListener('keydown', game.readKey.bind(game), false);
 
-        if(fixedInterval) {
-            clearInterval(fixedInterval);
-        }
-
-        if(updateInterval) {
+        if (updateInterval) {
             clearInterval(updateInterval);
         }
 
-        fixedInterval = setInterval(game.fixedUpdate.bind(game), 300);
-        updateInterval = setInterval(gameUpdate, 1000.0 / 30, ctx, game);
+        updateInterval = setInterval(gameUpdate, 1000.0 / 60, ctx, game, 1000.0 / 60);
     }
 }
